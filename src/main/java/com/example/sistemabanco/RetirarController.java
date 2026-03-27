5572e3097f60eddb3f2466d2c35720baeb20bb82
LizbethBernabe20 <20253rd057@utez.edu.mx>
2026-03-27 16:50:38 -0600

    Archivo Retirar

--- src/main/java/com/example/sistemabanco/RetirarController.java
+++ src/main/java/com/example/sistemabanco/RetirarController.java
@@ -1,4 +1,18 @@
 package com.example.sistemabanco;
 
 public class RetirarController {
+   private double cuenta;
+
+   public RetirarController(double cuenta){
+       this.cuenta = cuenta;
+   }
+   public String retirarDinero(double monto){
+       if (cuenta.retirar(monto)){
+           return "Retiro exitoso. Saldo : $" + cuenta.getSaldo();
+       }else{
+           return "Fondos insuficientes o monto invalido";
+       }
+   }
+
+
 }
